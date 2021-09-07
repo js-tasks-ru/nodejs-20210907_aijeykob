0a68c8ca167a9822159b18996e8670b6809a76f8
aijeykob <aijeykob@gmail.com>
2021-09-07 21:35:19 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,5 @@
-function sum(a, b) {
-  /* ваш код */
-}
-
-module.exports = sum;
+module.exports = (a, b) => !Number.isInteger(a) || !Number.isInteger(b) ?
+    (function() {
+      throw new TypeError;
+    })():
+    a+b;
