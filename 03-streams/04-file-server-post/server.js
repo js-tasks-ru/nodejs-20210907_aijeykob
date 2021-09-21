8f430d2122d5c0bdd3dfb2d58fea576c6b457e07
aijeykob <aijeykob@gmail.com>
2021-09-21 07:31:48 +0300

    full 03 streams finished

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,8 +1,10 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+
+const LimitSizeStream = require('./LimitSizeStream');
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -12,7 +14,41 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-
+      if (pathname.match(/\//)) {
+        res.statusCode = 400;
+        return res.end('Path cannot be nested');
+      }
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitSizeStream = new LimitSizeStream({limit: 10000});
+      req.pipe(limitSizeStream).pipe(writeStream);
+      writeStream.on('close', ()=> {
+        res.statusCode = 201;
+        res.end('file saved');
+      });
+      limitSizeStream.on('error', (error)=> {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end('file is too big');
+          writeStream.destroy();
+          return fs.unlink(filepath, (error)=>{});
+        }
+        res.statusCode = 500;
+        writeStream.destroy();
+        res.end('internal error');
+      });
+      writeStream.on('error', (error)=> {
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          return res.end('file already exist');
+        }
+        res.statusCode = 500;
+        res.end('internal error');
+      });
+      req.on('aborted', ()=> {
+        limitSizeStream.destroy();
+        writeStream.destroy();
+        fs.unlink(filepath, (err) => {});
+      });
       break;
 
     default:
