f9b7732f19649e353e0dca39a18762c9aec5339c
aijeykob <aijeykob@gmail.com>
2021-09-26 10:58:35 +0300

    add solution for 4-module 1-task

--- 04-testing-configuration-logging/01-unit-tests/Validator.js
+++ 04-testing-configuration-logging/01-unit-tests/Validator.js
@@ -1,14 +1,42 @@
 module.exports = class Validator {
   constructor(rules) {
     this.rules = rules;
+    this.rulesTypes = ['string', 'number'];
   }
 
   validate(obj) {
     const errors = [];
-
+    if (!obj) {
+      errors.push({field: 'obj', error: 'expect obj, got undefined'});
+      return errors;
+    }
     for (const field of Object.keys(this.rules)) {
       const rules = this.rules[field];
-
+      const validType = this.rulesTypes.includes(rules.type);
+      if (!validType) {
+        errors.push(
+            {field: 'type in constructor', error: `expect ${this.rulesTypes}, got ${rules.type}`});
+        break;
+      }
+      const validMinInteger = rules.min % 1 === 0;
+      const validMaxInteger = rules.max % 1 === 0;
+      if (!validMinInteger) {
+        errors.push({field: 'min in constructor', error: `expect integer, got ${rules.min}`});
+        break;
+      }
+      if (!validMaxInteger) {
+        errors.push({field: 'max in constructor', error: `expect integer, got ${rules.max}`});
+        break;
+      }
+      const validRange = rules.min <= rules.max;
+      if (!validRange) {
+        errors.push(
+            {
+              field: 'min in constructor',
+              error: `expect less or equal than ${rules.max}, got ${rules.min}`,
+            });
+        break;
+      }
       const value = obj[field];
       const type = typeof value;
 
@@ -27,6 +55,9 @@ module.exports = class Validator {
           }
           break;
         case 'number':
+          if (isNaN(value)) {
+            errors.push({field, error: `expect integer, got ${value}`});
+          }
           if (value < rules.min) {
             errors.push({field, error: `too little, expect ${rules.min}, got ${value}`});
           }
--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -12,11 +12,111 @@ describe('testing-configuration-logging/unit-tests', () => {
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+    });
+    it('test empty validate', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate();
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('obj');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect obj, got undefined');
+    });
+    it('test wrong rules type ', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string1',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({name: '1234567678'});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('type in constructor');
+      expect(errors[0]).to.have.property('error')
+          .and.to.be.equal('expect string,number, got string1');
+    });
+    it('test not integer max', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 1,
+          max: 1.1,
+        },
+      });
+
+      const errors = validator.validate({name: 1});
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('max in constructor');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect integer, got 1.1');
+    });
+    it('test valid min max range', () => {
+      const validator = new Validator({
+        age: {
+          type: 'string',
+          min: 10,
+          max: 5,
+        },
+      });
+
+      const errors = validator.validate({age: '12345678910'});
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('min in constructor');
+      expect(errors[0]).to.have.property('error')
+          .and.to.be.equal('expect less or equal than 5, got 10');
+    });
+    it('test min persist', () => {
+      const validator = new Validator({
+        age: {
+          type: 'string',
+          max: 10,
+        },
+      });
+
+      const errors = validator.validate({age: '12345678910'});
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('min in constructor');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect integer, got undefined');
+    });
+    it('test NaN', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 0,
+          max: 10,
+        },
+      });
+
+      const errors = validator.validate({age: NaN});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect integer, got NaN');
+    });
+    it('test correct data', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 0,
+          max: 2,
+        },
+      });
+
+      const errors = validator.validate({age: -0});
+      expect(errors).to.have.length(0);
     });
   });
 });
